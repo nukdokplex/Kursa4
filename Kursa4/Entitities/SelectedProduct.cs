935f6be4710b7b4fe1e046adb5bd514b1fbaa2db
NukDokPlex <vik.titoff2014@yandex.ru>
2021-03-02 14:00:31 +0500

    Добавлен рабочий выбор товаров. Исправлена привязка данных. Добавлен POJO для выбранных товаров.

---
+++ Kursa4/Entitities/SelectedProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursa4.Entitities
+{
+    public class SelectedProduct
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        public SelectedProduct(long id, string name, decimal price, int count)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Price = price;
+            this.Count = count;
+        }
+    }
+}
--- Kursa4/Windows/MakeOrderWindow.xaml.cs
+++ Kursa4/Windows/MakeOrderWindow.xaml.cs
@@ -1,24 +1,14 @@
-﻿using System;
+﻿using Kursa4.Entitities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Kursa4.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для MakeOrderWindow.xaml
-    /// </summary>
     public partial class MakeOrderWindow : Window
     {
         private const int GWL_STYLE = -16;
@@ -27,12 +17,18 @@ namespace Kursa4.Windows
         private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
         [DllImport("user32.dll")]
         private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        public List<SelectedProduct> SelectedProducts { get; set; }
         public MakeOrderWindow()
         {
             InitializeComponent();
             ReloadCustomers();
             ReloadAvailableProducts();
+            SelectedProducts = new List<SelectedProduct>();
+            SelectedProductsDataGrid.ItemsSource = SelectedProducts;
         }
+
+        
 
         private void ReloadCustomers()
         {
@@ -50,6 +46,12 @@ namespace Kursa4.Windows
             AvailableProductsDataGrid.ItemsSource = query.ToList();
         }
 
+        private void ReloadSelectedProducts()
+        {
+            SelectedProductsDataGrid.ItemsSource = SelectedProducts;
+            SelectedProductsDataGrid.Items.Refresh();
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             var hwnd = new WindowInteropHelper(this).Handle;
@@ -58,26 +60,120 @@ namespace Kursa4.Windows
 
         private void SelectProduct_Click(object sender, RoutedEventArgs e)
         {
+            if (AvailableProductsDataGrid.SelectedItem != null)
+            {
+                Product currentProduct = AvailableProductsDataGrid.SelectedItem as Product;
 
+                currentProduct = (from product in App.DB.Products
+                                  where product.ID == currentProduct.ID
+                                  select product).Single();
+                if (!SelectProductCount.Value.HasValue)
+                {
+                    MessageBox.Show(
+                        "Невалдиное значение количества выбираемого товара",
+                        "Внимание!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
+                    return;
+                }
+                if (currentProduct.Count < SelectProductCount.Value.Value)
+                {
+                    MessageBox.Show(
+                        "Значение количества выбранного товара превышает запасы на складе.",
+                        "Внимание!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                    );
+                    return;
+                }
+
+                for (int i = 0; i <= SelectedProducts.Count -1; i++)
+                {
+                    if (currentProduct.ID == SelectedProducts[i].ID)
+                    {
+                        SelectedProducts[i].Count += SelectProductCount.Value.Value;
+                        currentProduct.Count -= SelectProductCount.Value.Value;
+                        App.DB.SaveChanges();
+                        ReloadSelectedProducts();
+                        ReloadAvailableProducts();
+                        return;
+                    }
+                    continue;
+                }
+                
+                
+                SelectedProducts.Add(new SelectedProduct(
+                    currentProduct.ID, 
+                    currentProduct.Name, 
+                    currentProduct.Price, 
+                    SelectProductCount.Value.Value
+                    )
+                );
+                currentProduct.Count -= SelectProductCount.Value.Value;
+                App.DB.SaveChanges();
+                ReloadSelectedProducts();
+                ReloadAvailableProducts();
+                
+            }
         }
 
         private void DeselectProduct_Click(object sender, RoutedEventArgs e)
         {
+            //TODO Implement product deselection
+            /*if (SelectedProductsDataGrid.SelectedItem != null)
+            {
+                Product currentProduct = SelectedProductsDataGrid.SelectedItem as Product;
+                int index = -1;
 
+                for (int i = 0; i <= SelectedProducts.Count - 1; i++)
+                {
+                    if (SelectedProducts[i].ID == currentProduct.ID)
+                    {
+                        index = i;
+                        currentProduct = SelectedProducts[i];
+                        break;
+                    }
+                }
+
+                if (index == -1)
+                {
+                    return;
+                }
+
+                SelectedProducts.RemoveAt(index);
+                
+                for (int i = 0; i <= AvailableProductsDataGrid.Items.Count -1; i++)
+                {
+                    if (currentProduct.ID == (AvailableProductsDataGrid.Items[i] as Product).ID)
+                    {
+                        (from product in App.DB.Products
+                         where product.ID == currentProduct.ID
+                         select product).Single().Count += currentProduct.Count;
+                        break;
+                    }
+                }
+                App.DB.SaveChanges();
+                ReloadSelectedProducts();
+                ReloadAvailableProducts();
+            }*/
         }
 
         private void SaveExitButton_Click(object sender, RoutedEventArgs e)
         {
+            //TODO Implement order save
             this.Close();
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
+            //TODO Implement agreement MessageBox
             this.Close();
         }
 
         private void DeadlineAtField_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
+            //TODELETE This code isn't working at all
             DatePicker datePicker = sender as DatePicker;
             if (datePicker.SelectedDate.HasValue && datePicker.SelectedDate.Value > DateTime.Now)
             {
